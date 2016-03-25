2596672ccff32c261d86cf586c04608b2e922bee
Chiel Hackman <chielhackman@gmail.com>
2016-03-25 08:34:30 +0100

    Added comments to the JS file to make clear what the code is doing

--- calculator.js
+++ calculator.js
@@ -4,6 +4,7 @@
     this.executeArray = [];
   }
 
+// Method to update the display with the content of the executeArray.
   Calculator.prototype.updateDisplay = function() {
     document.getElementById(this.displayId).innerText = this.executeArray.join('');
   };
@@ -16,6 +17,13 @@
     return this.executeArray[this.executeArray.length - 1];
   };
 
+/*
+  Method to handle the number input
+  If the last item in the executeArray is a number, we concatenate it to the last item.
+  For example if the last item is the number 8 and the next input is a 7 then the value in the executeArray
+  will be 87.
+  If the last item in the executeArray is not a number than it's a operator so we can push the number to the executeArray.
+*/
   Calculator.prototype.handleNumber = function(number) {
     if (isNaN(this.getLastItem())) {
       this.executeArray.push(number.toString());
@@ -26,6 +34,12 @@
     this.updateDisplay();
   };
 
+/*
+  Method to handle the operator input.
+  First we check if the last item is a number, because 2 operators for example (++) don't make sense.
+  If the operator is a . (dot), we concatenate it to the last item.
+  If the operator is not a . (dot), we push it to the executeArray.
+*/
   Calculator.prototype.handleOperator = function(operator) {
     if (!isNaN(this.getLastItem())) {
       if (operator === '.') {
@@ -38,11 +52,17 @@
     }
   };
 
+/*
+  Method to clear the display. This will empty the executeArray.
+*/
   Calculator.prototype.allClear = function(clear) {
     this.executeArray = [];
     this.updateDisplay();
   };
 
+/*
+  Method for the backspace. This will delete the last item in the executeArray.
+*/
   Calculator.prototype.clearEntry = function() {
     this.executeArray[this.executeArray.length - 1] = this.getLastItem().toString().slice(0, -1);
     if (this.getLastItem().length < 1) {
@@ -51,6 +71,11 @@
     this.updateDisplay();
   };
 
+/*
+  Method to do the math.
+  If the last item in the executeArray is not a number we will remove it.
+  Then we evaluate (eval) the executeArray, put the result in the executeArray and update the display.
+*/
   Calculator.prototype.getTotal = function() {
     if (isNaN(this.getLastItem())) {
       this.executeArray.pop();
@@ -60,8 +85,14 @@
     this.updateDisplay();
   };
 
+/*
+  Initialize the calculator and give it the ID of the display element.
+*/
   var jsCalculator = new Calculator('display');
 
+/*
+  Bind the buttons to the actions.
+*/
   document.getElementById('ac').addEventListener('click', function() {
     jsCalculator.allClear();
   });
@@ -89,10 +120,20 @@
       });
     }
 
+/*
+  Method to use the keypad numbers and operators.
+*/
     window.onkeyup = function(e) {
     e.preventDefault();
     var key = e.keyCode ? e.keyCode : e.which;
 
+/*
+  If the key is a number it will run the handleNumber method.
+  If the key is a operator it will run the handleOperator method.
+  If the key is backspace it will run the clearEntry method.
+  If the key is delete it will run the allClear method.
+  If the key is enter it will run the getTotal method.
+*/
     if (key >= 96 && key <= 105) {
         jsCalculator.handleNumber(key - 96)
     } else if (key === 107) {
